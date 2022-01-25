1bc3471efc5eac91f2960ced517a380149010f7e
Baiksumin <bbb.41844@gmail.com>
2022-01-25 16:11:10 +0900

    changeTest

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world!!');
+  res.send('hello world!!><');
 });
 
 app.listen(4000);
